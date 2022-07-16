e4da4950862e249a4c49db2d3e8ac50249aff089
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2022-07-16 14:42:53 +0800

    update

---
+++ Robot SImulation/cek_coeffisient.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+xHeight = [0.2707, 0.2799, 0.2703, 0.2716, 0.29167, 0.634, 0.634, 0.6299, 0.6025, 0.6072, 0.1201, 0.1188, 0.0990, 0.1237, 0.1666]
+yHeight = [140, 140, 140, 140, 140, 90, 90, 90, 90, 90, 150, 150, 150, 150, 150]
+
+coffHeight = np.polyfit(xHeight, yHeight, 2)
+Ah, Bh, Ch = coffHeight
+print("Coeefisient ", Ah, Bh, Ch)
